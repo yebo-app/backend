01c62f039056f72a46d6ace1015da71b0d814964
Joydeep Mukherjee <joydeepm02@gmail.com>
2020-05-14 13:10:47 -0400

    Fixed IYP Link

--- yearbook/forms.py
+++ yearbook/forms.py
@@ -13,14 +13,27 @@ class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name','username', 'email', 'password1', 'password2']
+    """
+    def save(self, commit=True):
+        user = super().save(commit=False)
 
-class YearbookUserRegistrationForm(UserRegistrationForm):
+        user.first_name = self.cleaned_data('first_name')
+        user.last_name = self.cleaned_data('last_name')
+        user.email = self.cleaned_data('email')
+
+        if commit:
+            user.save()
+        return user
+    """
+
+class YearbookUserRegistrationForm(forms.ModelForm):
     class Meta:
         model = YearbookUser
-        exclude = ['username', 'email', 'password']
+        fields = ['bio']
 
 class SignatureForm(forms.ModelForm):
     class Meta:
