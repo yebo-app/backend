67b1a0928f706d0d5ce7cac01c8681938edcc022
smirk <kookygavinquack@gmail.com>
2020-06-27 20:21:31 -0400

    Re-adding the Heliohost server

--- digitalyearbook/settings.py
+++ digitalyearbook/settings.py
@@ -80,15 +80,15 @@ WSGI_APPLICATION = 'digitalyearbook.wsgi.application'
 
 DATABASES = {
 	'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'smirk_yebo2',
-        # 'USER': 'smirk_user',
-        # 'PASSWORD': 'password',
-        # 'HOST': '65.19.141.67',
-        # 'PORT': '3306',
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'smirk_yebo2',
+         'USER': 'smirk_user',
+         'PASSWORD': 'password',
+         'HOST': '65.19.141.67',
+         'PORT': '3306',
 
 		# 'ENGINE': 'django.db.backends.mysql',
 		# 'NAME': '',
